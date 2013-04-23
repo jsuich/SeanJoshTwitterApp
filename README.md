0a2f6ed4d413083773833e21d8ca7200b0338ac6
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-04-23 12:22:51 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Twitter Settings
+
+- Access level should be "Read and Write"
+- Callback URL *cannot* be blank.
+- Sign-in with Twitter should be "Yes".
+
+
---
+++ app/controllers/index.rb
@@ -0,0 +1,25 @@
+get '/' do
+  erb :index
+end
+
+get '/sign_in' do
+  # the `request_token` method is defined in `app/helpers/oauth.rb`
+  redirect request_token.authorize_url
+end
+
+get '/sign_out' do
+  session.clear
+  redirect '/'
+end
+
+get '/auth' do
+  # the `request_token` method is defined in `app/helpers/oauth.rb`
+  @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
+  # our request token is only valid until we use it to get an access token, so let's delete it from our session
+  session.delete(:request_token)
+
+  # at this point in the code is where you'll need to create your user account and store the access token
+
+  erb :index
+  
+end
